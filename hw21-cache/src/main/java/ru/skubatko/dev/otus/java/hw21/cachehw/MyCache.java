f0eb918f8b9fd321f099ff475e2e66a3457a2ffc
Sergey Kubatko <skubatko@gmail.com>
2020-12-22 00:55:38 +0300

    hw21 / week referenced listeners

--- hw21-cache/src/main/java/ru/skubatko/dev/otus/java/hw21/cachehw/MyCache.java
+++ hw21-cache/src/main/java/ru/skubatko/dev/otus/java/hw21/cachehw/MyCache.java
@@ -1,8 +1,10 @@
 package ru.skubatko.dev.otus.java.hw21.cachehw;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.WeakHashMap;
 
 /**
@@ -12,36 +14,39 @@ import java.util.WeakHashMap;
 public class MyCache<K, V> implements HwCache<K, V> {
 
     private final Map<K, V> cache = new WeakHashMap<>();
-    private final List<HwListener<K, V>> listeners = new ArrayList<>();
+    private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
 
 //Надо реализовать эти методы
 
     @Override
     public void put(K key, V value) {
         cache.put(key, value);
-        listeners.forEach(listener -> listener.notify(key, value, "put"));
+        listeners.forEach(reference -> Optional.ofNullable(reference.get())
+                                               .ifPresent(listener -> listener.notify(key, value, "put")));
     }
 
     @Override
     public void remove(K key) {
         V removed = cache.remove(key);
-        listeners.forEach(listener -> listener.notify(key, removed, "remove"));
+        listeners.forEach(reference -> Optional.ofNullable(reference.get())
+                                               .ifPresent(listener -> listener.notify(key, removed, "remove")));
     }
 
     @Override
     public V get(K key) {
         V value = cache.get(key);
-        listeners.forEach(listener -> listener.notify(key, value, "get"));
+        listeners.forEach(reference -> Optional.ofNullable(reference.get())
+                                               .ifPresent(listener -> listener.notify(key, value, "get")));
         return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        listeners.add(listener);
+        listeners.add(new WeakReference<>(listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        listeners.remove(listener);
+        listeners.remove(new WeakReference<>(listener));
     }
 }
