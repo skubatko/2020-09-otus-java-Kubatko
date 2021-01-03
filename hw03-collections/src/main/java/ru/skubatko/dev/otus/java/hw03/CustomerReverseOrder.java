4a593b9e1924469b3168ad37ecfab50971db5813
Sergey Kubatko <skubatko@gmail.com>
2021-01-03 19:41:44 +0300

    hw03-collections / initial setup

---
+++ hw03-collections/src/main/java/ru/skubatko/dev/otus/java/hw03/CustomerReverseOrder.java
@@ -0,0 +1,14 @@
+package ru.skubatko.dev.otus.java.hw03;
+
+public final class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+
+    public void add(Customer customer) {
+
+    }
+
+    public Customer take() {
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+}
---
+++ hw03-collections/src/main/java/ru/skubatko/dev/otus/java/hw03/CustomerService.java
@@ -0,0 +1,21 @@
+package ru.skubatko.dev.otus.java.hw03;
+
+import java.util.Map;
+
+public final class CustomerService {
+
+    //todo: 3. надо реализовать методы этого класса
+
+    public Map.Entry<Customer, String> getSmallest() {
+        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+
+    public void add(Customer customer, String data) {
+
+    }
+}
